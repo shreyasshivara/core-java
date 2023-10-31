c09f6e40111e8e53b5f84d089f7d9b7a589c11ef
shreyasshivara <149232974+shreyasshivara@users.noreply.github.com>
2023-10-31 11:54:59 +0530

    ShowroomStarter

---
+++ ShowroomStarter.java
@@ -0,0 +1,123 @@
+class ShowroomStarter{
+	
+	public static void main (String []car){
+		
+		
+		Showroom showroom= new Showroom();
+		System.out.println("showroom name:"+showroom.name);
+		System.out.println("showroom location:"+showroom.location);
+		System.out.println("showroom ratings:"+showroom.ratings);
+		System.out.println("showroom brand:"+showroom.brand);
+		System.out.println("showroom startedYear:"+showroom.startedYear);
+		System.out.println("showroom contactNo:"+showroom.contactNo);
+		System.out.println("showroom service:"+showroom.service);
+		System.out.println("showroom noOfStaffs:"+showroom.noOfStaffs);
+		
+		Showroom showroom1= new Showroom("Ravindu");
+		System.out.println("showroom1 name:"+showroom1.name);
+		System.out.println("showroom1 location:"+showroom1.location);
+		System.out.println("showroom1 ratings:"+showroom1.ratings);
+		System.out.println("showroom1 brand:"+showroom1.brand);
+		System.out.println("showroom1 startedYear:"+showroom1.startedYear);
+		System.out.println("showroom1 contactNo:"+showroom1.contactNo);
+		System.out.println("showroom1 service:"+showroom1.service);
+		System.out.println("showroom1 noOfStaffs:"+showroom1.noOfStaffs);
+		
+		
+		Showroom showroom2= new Showroom("Ravindu", "Hassan");
+		System.out.println("showroom2 name:"+showroom2.name);
+		System.out.println("showroom2 location:"+showroom2.location);
+		System.out.println("showroom2 ratings:"+showroom2.ratings);
+		System.out.println("showroom2 brand:"+showroom2.brand);
+		System.out.println("showroom2 startedYear:"+showroom2.startedYear);
+		System.out.println("showroom2 contactNo:"+showroom2.contactNo);
+		System.out.println("showroom2 service:"+showroom2.service);
+		System.out.println("showroom2 noOfStaffs:"+showroom2.noOfStaffs);
+		
+		Showroom showroom8= new Showroom("Varun", "Hebbal", 4.5D);
+		System.out.println("showroom8 name:"+showroom8.name);
+		System.out.println("showroom8 location:"+showroom8.location);
+		System.out.println("showroom8 ratings:"+showroom8.ratings);
+		System.out.println("showroom8 brand:"+showroom8.brand);
+		System.out.println("showroom8 startedYear:"+showroom8.startedYear);
+		System.out.println("showroom8 contactNo:"+showroom8.contactNo);
+		System.out.println("showroom8 service:"+showroom8.service);
+		System.out.println("showroom8 noOfStaffs:"+showroom8.noOfStaffs);
+		
+		
+		Showroom showroom3= new Showroom("Ravindu", "Hassan", 4.6D, "Toyota");
+		System.out.println("showroom3 name:"+showroom3.name);
+		System.out.println("showroom3 location:"+showroom3.location);
+		System.out.println("showroom3 ratings:"+showroom3.ratings);
+		System.out.println("showroom3 brand:"+showroom3.brand);
+		System.out.println("showroom3 startedYear:"+showroom3.startedYear);
+		System.out.println("showroom3 contactNo:"+showroom3.contactNo);
+		System.out.println("showroom3 service:"+showroom3.service);
+		System.out.println("showroom3 noOfStaffs:"+showroom3.noOfStaffs);
+		
+		Showroom showroom4= new Showroom("Mandovi", "BTM", 4.4D, "Suzuki", 2010);
+		System.out.println("showroom4 name:"+showroom4.name);
+		System.out.println("showroom4 location:"+showroom4.location);
+		System.out.println("showroom4 ratings:"+showroom4.ratings);
+		System.out.println("showroom4 brand:"+showroom4.brand);
+		System.out.println("showroom4 startedYear:"+showroom4.startedYear);
+		System.out.println("showroom4 contactNo:"+showroom4.contactNo);
+		System.out.println("showroom4 service:"+showroom4.service);
+		System.out.println("showroom4 noOfStaffs:"+showroom4.noOfStaffs);
+		
+		Showroom showroom5= new Showroom("Ravindu", "Hassan", 4.6D, "Toyota",2008, 9620416417L);
+		System.out.println("showroom5 name:"+showroom5.name);
+		System.out.println("showroom5 location:"+showroom5.location);
+		System.out.println("showroom5 ratings:"+showroom5.ratings);
+		System.out.println("showroom5 brand:"+showroom5.brand);
+		System.out.println("showroom5 startedYear:"+showroom5.startedYear);
+		System.out.println("showroom5 contactNo:"+showroom5.contactNo);
+		System.out.println("showroom5 service:"+showroom5.service);
+		System.out.println("showroom5 noOfStaffs:"+showroom5.noOfStaffs);
+		
+		Showroom showroom6= new Showroom("Ravindu", "Hassan", 4.6D, "Toyota",2008, 9620416417L, true);
+		System.out.println("showroom6 name:"+showroom6.name);
+		System.out.println("showroom6 location:"+showroom6.location);
+		System.out.println("showroom6 ratings:"+showroom6.ratings);
+		System.out.println("showroom6 brand:"+showroom6.brand);
+		System.out.println("showroom6 startedYear:"+showroom6.startedYear);
+		System.out.println("showroom6 contactNo:"+showroom6.contactNo);
+		System.out.println("showroom6 service:"+showroom6.service);
+		System.out.println("showroom6 noOfStaffs:"+showroom6.noOfStaffs);
+		
+		Showroom showroom7= new Showroom("Ravindu", "Hassan", 4.6D, "Toyota",2008, 9620416417L, true,40);
+		System.out.println("showroom7 name:"+showroom7.name);
+		System.out.println("showroom7 location:"+showroom7.location);
+		System.out.println("showroom7 ratings:"+showroom7.ratings);
+		System.out.println("showroom7 brand:"+showroom7.brand);
+		System.out.println("showroom7 startedYear:"+showroom7.startedYear);
+		System.out.println("showroom7 contactNo:"+showroom7.contactNo);
+		System.out.println("showroom7 service:"+showroom7.service);
+		System.out.println("showroom7 noOfStaffs:"+showroom7.noOfStaffs);
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
