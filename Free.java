374d33e3b38d47463ad643aa007119db70cebd15
shreyasshivara <149232974+shreyasshivara@users.noreply.github.com>
2023-11-03 20:32:34 +0530

    Free

---
+++ Free.java
@@ -0,0 +1,38 @@
+class Free{
+	
+	String what;
+	String why;
+	String freeForAll;
+	
+	
+	Free(String what,String why,String freeForAll){
+		this.what=what;
+		this.why=why;
+		this.freeForAll=freeForAll;
+		
+	}
+	void DisplayInfo()
+	{
+		System.out.println("The free cource is:"+ this.what);
+		System.out.println("Free Because:"+ this.why);
+		System.out.println("The offer is:"+ this.freeForAll);
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
