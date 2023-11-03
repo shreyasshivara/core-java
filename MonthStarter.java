b3c4b7955c42acf9251ac913b6ec2daf90116c9b
shreyasshivara <149232974+shreyasshivara@users.noreply.github.com>
2023-11-03 20:39:30 +0530

    MonthStarter

---
+++ MonthStarter.java
@@ -0,0 +1,58 @@
+class MonthStarter{
+	
+	public static void main(String[]okey)
+	{
+		Month month=new Month("January",31,5,7);
+		Month month1=new Month("feb",29,4,6);
+		Month month2=new Month("march",31,4,6);
+		Month month3=new Month("april",30,4,8);
+		Month month4=new Month("may",29,4,7);
+		Month month5=new Month("june",31,5,9);
+		Month month6=new Month("july",30,4,5);
+		Month month7=new Month("august",31,5,8);
+		Month month8=new Month("september",29,4,10);
+		Month month9=new Month("october",31,5,11);
+		Month month10=new Month("november",30,4,5);
+		Month month11=new Month("December",31,5,9);
+		month.DisplayInfo();
+		System.out.println("\n");
+		month1.DisplayInfo();
+		System.out.println("\n");
+		month2.DisplayInfo();
+		System.out.println("\n");
+		
+		month3.DisplayInfo();
+		System.out.println("\n");
+		month4.DisplayInfo();
+		System.out.println("\n");
+		
+		month5.DisplayInfo();
+		System.out.println("\n");
+		month6.DisplayInfo();
+		System.out.println("\n");
+		month7.DisplayInfo();
+		System.out.println("\n");
+		month8.DisplayInfo();
+		System.out.println("\n");
+		month9.DisplayInfo();
+		System.out.println("\n");
+		month10.DisplayInfo();
+		System.out.println("\n");
+		month11.DisplayInfo();
+		
+		
+		
+		
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
