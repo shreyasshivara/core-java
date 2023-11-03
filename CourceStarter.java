02ae22ee3fe82fe664a357ca66bad31e3b587bae
shreyasshivara <149232974+shreyasshivara@users.noreply.github.com>
2023-11-03 20:32:05 +0530

    CourceStarter

---
+++ CourceStarter.java
@@ -0,0 +1,61 @@
+class CourceStarter{
+	public static void main (String[]read)
+	{
+		System.out.println("Starting main in CourceStarter");
+		
+		
+		Cource cource=new Cource("BE", "ATMECE", 75000, "Mysore", "good", false,4);
+		Cource cource1=new Cource("BE", "jc", 100000, "Mysore", "good", true,4);
+		Cource cource2=new Cource("BE", "RNS", 95000, "Banglore", "good", true,4);
+		Cource cource3=new Cource("BE", "MCE", 86000, "Hassan", "good", true,4);
+		Cource cource4=new Cource("BE", "MIT", 90000, "Mysore", "good", false,4);
+		Cource cource5=new Cource("Diploma", "LVP", 5000, "Hassan", "good", false,3);
+		Cource cource6=new Cource("BE", "Rajeev", 80000, "Hassan", "good", true,4);
+		Cource cource7=new Cource("BCA", "maharaja", 30000, "Mysore", "good", false,3);
+		Cource cource8=new Cource("BE", "UBDT", 76000, "DAvanagere", "good", true,4);
+		Cource cource9=new Cource("BE", "NDRK", 90000, "Hassan", "good", false,4);
+		Cource cource10=new Cource("BE", "NIE", 115000, "Mysore", "good", true,4);
+		Cource cource11=new Cource("BE", "RV", 130000, "Banglore", "good", true,4);
+		cource.DisplayInfo();
+		System.out.println("\n");
+		cource1.DisplayInfo();
+		System.out.println("\n");
+		cource2.DisplayInfo();
+		System.out.println("\n");
+		cource3.DisplayInfo();
+		System.out.println("\n");
+		cource4.DisplayInfo();
+		System.out.println("\n");
+		cource5.DisplayInfo();
+		System.out.println("\n");
+		cource6.DisplayInfo();
+		System.out.println("\n");
+		cource7.DisplayInfo();
+		System.out.println("\n");
+		cource8.DisplayInfo();
+		System.out.println("\n");
+		cource9.DisplayInfo();
+		System.out.println("\n");
+		cource10.DisplayInfo();
+		System.out.println("\n");
+		cource11.DisplayInfo();
+		
+		
+		
+		
+		System.out.println("Ending main in CourceStarter");
+		
+		
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
